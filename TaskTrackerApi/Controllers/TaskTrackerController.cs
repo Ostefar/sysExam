c4e11415ecda8b4dbdda8bc89f476d719daf201a
Ostefar <56957832+Ostefar@users.noreply.github.com>
2023-12-19 11:08:43 +0100

    test AB#34

---
+++ TaskTrackerApi/Controllers/TaskTrackerController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TaskTrackerApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TaskTrackerController : ControllerBase
+    {
+    }
+}
